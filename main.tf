eb0050e13dd1f8f3df20e43041b5916209305319
Theo McCaie <theo.mccaie@informaticslab.co.uk>
2017-03-31 13:15:43 +0100

    wip

--- main.tf
+++ main.tf
@@ -4,17 +4,10 @@ provider "aws" {
 
 data "template_file" "bootstrap" {
   template = "${file("boot.tlp")}"
-  vars = {
-    DATAPOINT_KEY="${var.DATAPOINT_KEY}"
-    WEATHERSPARK_C_TOKEN="${var.WEATHERSPARK_C_TOKEN}"
-    WEATHERSPARK_C_SECRET="${var.WEATHERSPARK_C_SECRET}"
-    WEATHERSPARK_A_TOKEN="${var.WEATHERSPARK_A_TOKEN}"
-    WEATHERSPARK_A_SECRET="${var.WEATHERSPARK_A_SECRET}"
-  }
 }
 
 
-resource "aws_instance" "theosparkline" {
+resource "aws_instance" "techconnectgames" {
   ami                   = "ami-d41d58a7"
   instance_type         = "t2.micro"
   key_name              = "gateway"
