e6d17db6cbe553506fe8cc97f631d21bf131ad4b
Theo McCaie <theo.mccaie@informaticslab.co.uk>
2017-04-01 15:42:23 +0100

    start adding connect 4

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var xandos = require('./routes/xandos');
+var conn4 = require('./routes/conn4');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/xandos', xandos);
+app.use('/conn4', conn4);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ public/javascripts/conn4.js
@@ -0,0 +1,121 @@
+/*
+   0 1 2
+A |_|_|_|
+B |_|_|_|
+C |_|_|_|
+*/
+
+const X = 'X';
+const Y = 'Y';
+const NONE = '-';
+var state = NONE.repeat(9);
+// socket
+var socket = io.connect(location.protocol + '//' + location.host);
+socket.on('state', function (data) {
+  console.log('got state ', data);
+  if(data.c4state && data.c4state != state){
+    state = data.c4state;
+    update();
+    showState();
+  }
+});
+function sendState(){
+  console.log('send state ', state);
+  socket.emit('stateUpdate', { c4state: state });
+}
+
+
+function showState(){
+  document.getElementById('state-code').innerHTML = state;
+}
+
+function cellNumToRef(i){
+  let col = (i % 7)
+  let row = 'ABCDEFG'[Math.floor(i/7)];
+  return [col, row];
+}
+
+function checkAndMakeMove(e){
+  e.preventDefault();
+  try {
+    let player = document.getElementById('player').value.toUpperCase();
+    let move = document.getElementById('move').value.toUpperCase().replace(/[ )(]/g,'').split(',');
+    move[0] = Number(move[0]);
+    makeMove(move, player);
+  } catch(e){
+    alert('Bad move or something went wrong!');
+  }
+}
+
+function makeMove(move, player){
+  let index = moveToStateIndex(move);
+  if(state[index] !== NONE){
+    throw {msg:'Illegal Move', 'move':move};
+  }
+  state = (state.substr(0, index) + player + state.substr(index)).substr(0,9);
+  showState();
+  update();
+  sendState();
+}
+
+function update(){
+  const ximg = "<img src='/images/x.svg' class='cell-mark' alt='X'>";
+    const oimg = "<img src='/images/o.svg' class='cell-mark' alt='O'>";
+  for (let i=0; i<9; i++){
+    let id = 'state-' + String(cellNumToRef(i));
+    let ele = document.getElementById(id);
+    if (state[i] != NONE){
+      ele.innerHTML = (state[i] == X)? ximg : oimg;
+    }
+  }
+}
+
+function moveToStateIndex(move){
+  col = move[0];
+  row = {'A':0, 'B':1, 'C':2}[move[1]];
+  return col + row * 3;
+}
+
+
+function buildBoard(){
+
+  function newRow(container){
+    let row = document.createElement('div');
+    row.className ='row';
+    container.appendChild(row);
+    return row;
+  }
+
+  let container = document.getElementById('game');
+  let row = newRow(container);
+  for (let i=0; i<7*6; i++){
+    if (i % 7 == 0){
+      row = newRow(container);
+    }
+    let cell = document.createElement('div');
+    cell.id = String(cellNumToRef(i));
+    cell.innerHTML = '<p class="ref">'+cell.id+'</p><div class="cell-state" id="state-'+cell.id+'"></div>';
+    cell.className = 'cell col-xs-1';
+    row.appendChild(cell);
+  }
+  setInterval(scaleCellsVertically, 200);
+}
+
+function scaleCellsVertically(){
+  let cells = document.getElementsByClassName('cell');
+  for (var i = 0; i < cells.length; i++) {
+    let cell = cells[i];
+    cell.style.height = cell.clientWidth + 'px';
+  }
+}
+
+function showControls(){
+  document.getElementById('controls').style.display = 'block'
+}
+
+
+
+
+buildBoard();
+showState();
+document.getElementById('controls').addEventListener('submit', checkAndMakeMove, false)
